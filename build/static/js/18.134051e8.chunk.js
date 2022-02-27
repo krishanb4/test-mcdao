359fc2b3bfd3484913b9d3dfda2819dba1abab4c
krishanb4 <krishanbandara4444@gmail.com>
2022-02-27 14:02:07 +0530

    build

---
+++ build/static/js/18.134051e8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[18],{1112:function(n,e,a){"use strict";a.r(e);var t=a(936);e.default=t.Line}}]);
+//# sourceMappingURL=18.134051e8.chunk.js.map
---
+++ build/static/js/20.0ca8d1cb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[20],{960:function(n,p){}}]);
+//# sourceMappingURL=20.0ca8d1cb.chunk.js.map
